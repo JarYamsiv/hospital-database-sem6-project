7cd0823ee7fffa33fba92d6c430fbccbab2abedd
vismay <JarYamsiv>
2019-02-11 22:50:39 +0530

    view.php working fine adn also supports tables

--- gui/viewInterface/Patient/view.php
+++ gui/viewInterface/Patient/view.php
@@ -5,7 +5,7 @@
  	$password = "mynewpassword";
  	$dbname = "hospital";
 
- 	echo "connecting...<br>";
+ 	//echo "connecting...<br>";
 	// Create connection
 	 $conn = mysqli_connect($servername, $username, $password , $dbname);
 
@@ -15,19 +15,45 @@
 
 	 }
 
-	 echo "connceted<br>";
+	 echo "<h1>Patient Data</h1><br>";
 
 
 	 $sql = "SELECT * FROM Patient";
 	 $result = mysqli_query($conn, $sql);
 
-	if (mysqli_num_rows($result) > 0) {
-    	// output data of each row
+	if (mysqli_num_rows($result) > 0) 
+	{
+    	// output data of each row]
+    	echo "
+    	<table border=\"1\" cellpadding=\"10\">
+
+    	<tr>
+    	<td>ID</td>
+    	<td>Name</td>
+    	<td>Age</td>
+    	<td>LastVisit</td>
+    	<td>Address</td>
+    	<td>MedicalHistory</td>
+    	</tr>
+
+    	";
     	while($row = mysqli_fetch_assoc($result)) {
-        	echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Age"]. "<br>";
+    		echo "<tr>";
+    		echo "<td>".$row["ID"]."</td>";
+    		echo "<td>".$row["Name"]."</td>";
+    		echo "<td>".$row["Age"]."</td>";
+    		echo "<td>".$row["Last_visit"]."</td>";
+    		echo "<td>".$row["Address"]."</td>";
+    		echo "<td>".$row["Medical_history"]."</td>";
+        	//echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " " . $row["Age"]. "<br>";
+        	echo "</tr>";
     		}
-		} 		else {
-    	echo "0 results";
+
+    	echo "</table>";
+	} 		
+	else 
+	{
+    		echo "0 results";
 	}
 
 	 mysqli_close($conn);
